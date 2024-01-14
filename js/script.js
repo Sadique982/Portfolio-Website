22bbdf996ad24b596ebfecf54fda99c9448d0e62
Mohammed Sadique <84969756+Sadique982@users.noreply.github.com>
2024-01-14 23:40:51 +0530

    BW Mode remeberence
    Make it such that it remembers which mode the dark or light user has chosen.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
 function BWmode() {
    var element = document.body;
    element.classList.toggle("light-mode");
+
 }
+document.addEventListener('DOMContentLoaded', function () {
+   var isLightModeToggled = localStorage.getItem('lightModeToggled') === 'true';
+   var element = document.body;
+   if (isLightModeToggled) {
+      element.classList.add("light-mode");
+   } else {
+      element.classList.remove("light-mode");
+   }
+   element.addEventListener('click', function (event) {
+      if (!event.target.closest('main') && event.target.closest('label')) {
+         element.classList.toggle("light-mode");
+         localStorage.setItem('lightModeToggled', element.classList.contains('light-mode'));
+      }
+   });
+});
